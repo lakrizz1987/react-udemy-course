fb906a30a265988bac9dcfff60b23ce50e73aa60
Ivaylo <ignatov.ivaylo@gmail.com>
2022-09-21 13:42:51 +0300

    create submit handler

--- 2-React state-Working whith events/src/components/NewExpense/ExpenseForm.js
+++ 2-React state-Working whith events/src/components/NewExpense/ExpenseForm.js
@@ -18,8 +18,21 @@ const ExpenseForm = () => {
         setEnteredDate(e.target.value)
     }
 
+    function submitHandler(e){
+        e.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+       
+        console.log(expenseData)
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
