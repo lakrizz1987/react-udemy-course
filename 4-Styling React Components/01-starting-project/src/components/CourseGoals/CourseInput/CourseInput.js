cdf4f483bba37717c2febf6b3e6a197ef2393bcb
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-09-23 15:02:30 +0300

    add inline styles for empty input

--- 4-Styling React Components/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 4-Styling React Components/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -4,22 +4,29 @@ import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
+  const [isValid, setIsValid] = useState(false);
   const [enteredValue, setEnteredValue] = useState('');
 
   const goalInputChangeHandler = event => {
+    setIsValid(false);
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+
+    if (enteredValue.length === 0) {
+      setIsValid(true);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input style={{ 'borderColor': isValid ? 'red' : 'black' }} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
