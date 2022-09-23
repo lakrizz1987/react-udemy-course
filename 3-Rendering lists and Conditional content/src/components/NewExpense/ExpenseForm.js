2b3aa2dec4ee7301a6af3938dfd30ce9157672fe
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-09-23 11:47:27 +0300

    lift state to show and hide form

--- 3-Rendering lists and Conditional content/src/components/NewExpense/ExpenseForm.js
+++ 3-Rendering lists and Conditional content/src/components/NewExpense/ExpenseForm.js
@@ -3,23 +3,24 @@ import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
 
+
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
 
-    function titleOnChangeHandler(e){
+    function titleOnChangeHandler(e) {
         setEnteredTitle(e.target.value);
     }
 
-    function amountOnChangeHandler(e){
+    function amountOnChangeHandler(e) {
         setEnteredAmount(e.target.value)
     }
 
-    function dateOnChangeHandler(e){
+    function dateOnChangeHandler(e) {
         setEnteredDate(e.target.value)
     }
 
-    function submitHandler(e){
+    function submitHandler(e) {
         e.preventDefault();
 
         const expenseData = {
@@ -33,26 +34,29 @@ const ExpenseForm = (props) => {
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
-       
+
     };
+
+
 
     return (
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' value={enteredTitle} onChange={titleOnChangeHandler}/>
+                    <input type='text' value={enteredTitle} onChange={titleOnChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number'  value={enteredAmount} min='0.01' step='0.01' onChange={amountOnChangeHandler} />
+                    <input type='number' value={enteredAmount} min='0.01' step='0.01' onChange={amountOnChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' max='2022-12-31'  value={enteredDate} onChange={dateOnChangeHandler} />
+                    <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateOnChangeHandler} />
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button onClick={() => props.setView(false)}>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
--- 3-Rendering lists and Conditional content/src/components/NewExpense/NewExpense.js
+++ 3-Rendering lists and Conditional content/src/components/NewExpense/NewExpense.js
@@ -1,22 +1,27 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+    const [isShow, setIsShow] = useState(false);
 
-    function saveExpenseDataHandler(data){
+    function saveExpenseDataHandler(data) {
         const expenseData = {
             ...data,
-            id:Math.random().toString()
+            id: Math.random().toString()
         };
         props.onSaveExpenseData(expenseData);
+        setIsShow(false)
     }
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveEnteredData={saveExpenseDataHandler}/>
+            {isShow ? <ExpenseForm setView={setIsShow} onSaveEnteredData={saveExpenseDataHandler} /> :  <button onClick={() => setIsShow(true)}>Add Expense</button>}
+           
+            
         </div>
     );
-    
+
 };
 
 export default NewExpense;
