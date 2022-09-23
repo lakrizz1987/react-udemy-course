bb9cda2c39b077bbda512c8f7ecc167c24e76216
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-09-23 10:57:45 +0300

    create no content message

--- 3-Rendering lists and Conditional content/src/components/Expenses/Expenses.js
+++ 3-Rendering lists and Conditional content/src/components/Expenses/Expenses.js
@@ -6,32 +6,36 @@ import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
-  const [selectedYear, setSelectedYear] = useState('2020');
+    const [selectedYear, setSelectedYear] = useState('2020');
 
-  function changeHandlerExpense(selectedYear) {
-    setSelectedYear(selectedYear)
-  }
+    function changeHandlerExpense(selectedYear) {
+        setSelectedYear(selectedYear)
+    }
 
-  const fiteredByYearData = props.items.filter(x => {
-    return x.date.getFullYear().toString() === selectedYear;
-  })
+    const fiteredByYearData = props.items.filter(x => {
+        return x.date.getFullYear().toString() === selectedYear;
+    })
 
-  return (
-    <div>
-      <Card className="expenses">
 
-        <ExpensesFilter selected={selectedYear} onChangeYear={changeHandlerExpense} />
+    let renderingContent = <p style={{ 'color': 'white' }}>No expenses for this preiod!</p>
 
-        {fiteredByYearData.map(expense => <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />)}
+    if (fiteredByYearData.length > 0) {
+        renderingContent = fiteredByYearData.map(expense => <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+        />);
+    };
 
-      </Card>
-    </div>
-  );
+    return (
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter selected={selectedYear} onChangeYear={changeHandlerExpense} />
+                {renderingContent}
+            </Card>
+        </div>
+    );
 }
 
 export default Expenses;
