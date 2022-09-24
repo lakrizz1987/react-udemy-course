48b2063ad8550d53a2145e4aacfd3ecc86f1891a
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-09-24 17:54:53 +0300

    create button component

--- 6-Time to practise/01-starting-project/src/Components/Users/AddUser.js
+++ 6-Time to practise/01-starting-project/src/Components/Users/AddUser.js
@@ -1,15 +1,33 @@
+import { useState } from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 
 const AddUser = () => {
+    const [eneteredName, setEnteredName] = useState('');
+    const [eneteredAge, setEnteredAge] = useState('');
 
     function addUserHandler(e) {
         e.preventDefault();
+        
         const formData = new FormData(e.currentTarget);
         const name = formData.get('username');
         const age = formData.get('age');
+
+        if (name.length.trim() === 0 || age.length.trim() === 0) {
+            return;
+        }
+
+        if (Number(age) < 0) {
+            return;
+        }
+
+        setEnteredName(name);
+        setEnteredAge(age);
         e.target.reset();
+
     }
+
 
     return (
         <Card className={styles.input}>
@@ -18,7 +36,7 @@ const AddUser = () => {
                 <input type='text' id="username" name="username" />
                 <label htmlFor="Age">Age</label>
                 <input type='number' id="Age" name="age" />
-                <button type="submit">Add user</button>
+                <Button type="submit">Add user</Button>
             </form>
         </Card>
     )
