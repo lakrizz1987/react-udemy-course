5857f3530b5dc3d0c68e48e8db7c12fd9c3e017e
Ivaylo <ignatov.ivaylo@gmail.com>
2022-09-30 13:05:37 +0300

    create HeaderCartButton animation

--- 8-Practice/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ 8-Practice/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,25 +1,45 @@
-import { useContext } from 'react';
-import CartContext from '../../store/cart-context';
+import { useContext, useEffect, useState } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
-import styles from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
+import classes from './HeaderCartButton.module.css';
 
-function HeaderCartButton(props) {
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
 
-    const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
-        return  curNumber + item.amount
-    }, 0)
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
-    return (
-        <button className={styles.button} onClick={props.onClick}>
-            <span className={styles.icon}>
-                <CartIcon />
-            </span>
-            <span>Your Cart</span>
-            <span className={styles.badge}>{numberOfCartItems}</span>
-        </button>
-    )
-}
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
 
 export default HeaderCartButton;
