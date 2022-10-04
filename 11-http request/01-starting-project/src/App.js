cffd4c543ccdee99fda5ac5e3974490940744eac
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-04 10:50:15 +0300

    get request to Api to get movies

---
+++ 11-http request/01-starting-project/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([])
+
+  function getMoviesHandler() {
+    fetch('https://swapi.dev/api/films')
+      .then(res => res.json())
+      .then(data => setMovies(data.results))
+  }
+
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={getMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
