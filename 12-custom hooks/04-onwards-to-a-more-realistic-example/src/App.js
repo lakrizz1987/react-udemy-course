fa3d965b8d3a3bc9e6d79b391df7983d75e23bb8
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-06 11:10:47 +0300

    create hook for get request

--- 12-custom hooks/04-onwards-to-a-more-realistic-example/src/App.js
+++ 12-custom hooks/04-onwards-to-a-more-realistic-example/src/App.js
@@ -1,43 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/useHttp';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+
+  const addTaskHandler = useCallback((data) => {
+    const loadedTasks = [];
+    for (const taskKey in data) {
+      loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+    }
+    setTasks(loadedTasks);
+  }, [])
+
+  const { isLoading, error, fetchTasks } = useHttp(addTaskHandler);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://speedy-lattice-91920-default-rtdb.firebaseio.com/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
---
+++ 12-custom hooks/04-onwards-to-a-more-realistic-example/src/hooks/useHttp.js
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from "react";
+
+const useHttp = (handler) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchTasks = useCallback( async (methods) => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(
+                'https://speedy-lattice-91920-default-rtdb.firebaseio.com/tasks.json',
+                 methods ? methods : {}
+            );
+
+            if (!response.ok) {
+                throw new Error('Request failed!');
+            }
+
+            const data = await response.json();
+            handler(data)
+
+        } catch (err) {
+            setError(err.message || 'Something went wrong!');
+        }
+        setIsLoading(false);
+    },[handler]);
+
+    useEffect(() => {
+        fetchTasks();
+    }, [fetchTasks]);
+
+    return { isLoading, error, fetchTasks }
+};
+
+export default useHttp;
