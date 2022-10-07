df009fbf9dbd70fda59f20d2aaa1c8a97f59b48d
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-07 12:02:33 +0300

    create validation for the input

--- 13-working-with-forms/01-starting-project/src/components/SimpleInput.js
+++ 13-working-with-forms/01-starting-project/src/components/SimpleInput.js
@@ -3,23 +3,29 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const inputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
+  const [nameIsvalid, setNameIsValid] = useState(true);
 
   const inputOnChangeHandler = (e) => {
     setEnteredName(e.target.value);
   }
 
-  const onSubmitHandler =(e)=>{
+  const onSubmitHandler = (e) => {
     e.preventDefault();
+    if (inputRef.current.value === '') {
+      setNameIsValid(false)
+      return
+    }
     console.log(inputRef.current.value)
   }
 
-  
+  const divClass = nameIsvalid ? 'form-control': 'form-control invalid'
 
   return (
     <form onSubmit={onSubmitHandler}>
-      <div className='form-control'>
+      <div className={divClass}>
         <label htmlFor='name'>Your Name</label>
         <input ref={inputRef} type='text' id='name' onChange={inputOnChangeHandler} />
+        {!nameIsvalid && <p className="error-text">All fields are require.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
