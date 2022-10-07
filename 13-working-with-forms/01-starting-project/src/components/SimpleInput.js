277309863c5e80928c5d7b13f6a8fd3b43d15faf
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-07 12:15:38 +0300

    creating state to manage if form is touched

--- 13-working-with-forms/01-starting-project/src/components/SimpleInput.js
+++ 13-working-with-forms/01-starting-project/src/components/SimpleInput.js
@@ -3,7 +3,10 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const inputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [nameIsvalid, setNameIsValid] = useState(true);
+  const [nameIsvalid, setNameIsValid] = useState(false);
+  const [formIsTouched, setFormIsTouched] = useState(false);
+
+  let inputIsInvalid = formIsTouched && !nameIsvalid;
 
   const inputOnChangeHandler = (e) => {
     setEnteredName(e.target.value);
@@ -11,21 +14,24 @@ const SimpleInput = (props) => {
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
+    setFormIsTouched(true);
+
     if (inputRef.current.value === '') {
       setNameIsValid(false)
       return
     }
+    setNameIsValid(true)
     console.log(inputRef.current.value)
   }
 
-  const divClass = nameIsvalid ? 'form-control': 'form-control invalid'
+  const divClass = inputIsInvalid ? 'form-control invalid' : 'form-control'
 
   return (
     <form onSubmit={onSubmitHandler}>
       <div className={divClass}>
         <label htmlFor='name'>Your Name</label>
         <input ref={inputRef} type='text' id='name' onChange={inputOnChangeHandler} />
-        {!nameIsvalid && <p className="error-text">All fields are require.</p>}
+        {inputIsInvalid && <p className="error-text">All fields are require.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
