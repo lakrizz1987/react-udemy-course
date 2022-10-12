3871eee94ea1ee9d8d892a13e6366741392450ee
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-10-12 21:01:55 +0300

    Update redux.intro.js

--- 15-redux/intro/redux.intro.js
+++ 15-redux/intro/redux.intro.js
@@ -1,12 +1,19 @@
 const redux = require('redux');
 
 const counterReducer = (state = { counter: 0 }, action) => {
-    if(action.type === 'increment'){
+    if (action.type === 'increment') {
         return {
             counter: state.counter + 1
         }
     }
-    
+
+    if (action.type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    return state;
 }
 
 const store = redux.createStore(counterReducer);
@@ -19,3 +26,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'decrement' })
