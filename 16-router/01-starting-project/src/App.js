3d8001074fadda84dd1b8e73b7bbee938b4630b8
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-20 14:42:39 +0300

    Redirect component

--- 16-router/01-starting-project/src/App.js
+++ 16-router/01-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from './components/MainHeader'
@@ -13,6 +13,9 @@ function App() {
       </header>
       <main>
         <Switch>
+          <Route path='/' exact>
+            <Redirect to='/welcome'/>
+          </Route>
           <Route path='/welcome'>
             <Welcome />
           </Route>
