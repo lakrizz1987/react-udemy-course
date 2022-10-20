304e6174a7d850c627ecb0dbc64d0972959ebdd8
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-20 16:16:12 +0300

    useParams

--- 16-router/09-time-to-practice-starting-code/src/pages/QuoteDetails.js
+++ 16-router/09-time-to-practice-starting-code/src/pages/QuoteDetails.js
@@ -1,6 +1,14 @@
-const QuoteDetails = () =>{
-    return(
-        <h1>Quote Details</h1>
+import { Fragment } from "react";
+import { useParams } from "react-router-dom";
+
+const QuoteDetails = () => {
+    const params = useParams();
+
+    return (
+        <Fragment>
+            <h1>Quote Details</h1>
+            <p>{params.id}</p>
+        </Fragment>
     )
 };
 
