7ac2dc1da23604aabf1af179e3a3273148f25eec
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-10-20 17:44:41 +0300

    add nested route in details component

--- 16-router/09-time-to-practice-starting-code/src/pages/QuoteDetails.js
+++ 16-router/09-time-to-practice-starting-code/src/pages/QuoteDetails.js
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 import { useParams } from "react-router-dom";
+import { Route } from 'react-router-dom';
+import Comments from "../components/comments/Comments";
 
 const QuoteDetails = () => {
     const params = useParams();
@@ -8,6 +10,9 @@ const QuoteDetails = () => {
         <Fragment>
             <h1>Quote Details</h1>
             <p>{params.id}</p>
+            <Route path={`/quotes/${params.id}/comments`}>
+                <Comments />
+            </Route>
         </Fragment>
     )
 };
