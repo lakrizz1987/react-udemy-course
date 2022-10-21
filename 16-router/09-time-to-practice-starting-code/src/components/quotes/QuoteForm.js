f88d5943d19f4f7849260ecc4e66011188895bd3
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-21 08:57:56 +0300

    redirect with useHistory when add quote

--- 16-router/09-time-to-practice-starting-code/src/components/quotes/QuoteForm.js
+++ 16-router/09-time-to-practice-starting-code/src/components/quotes/QuoteForm.js
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
 
+
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
@@ -7,6 +8,7 @@ import classes from './QuoteForm.module.css';
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+  
 
   function submitFormHandler(event) {
     event.preventDefault();
--- 16-router/09-time-to-practice-starting-code/src/pages/CreateQuote.js
+++ 16-router/09-time-to-practice-starting-code/src/pages/CreateQuote.js
@@ -1,10 +1,14 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 
 const CreateQuotes = () => {
-    
+    const history = useHistory();
+
     const onAddHandler = (data) => {
         console.log(data)
+        history.push('/quotes')
+
     }
 
     return (
