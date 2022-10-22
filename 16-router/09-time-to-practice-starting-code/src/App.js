634b9a955fda5b791cad7446006f01d4b9b95b20
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-10-22 12:37:59 +0300

    using React.lazy

--- 16-router/09-time-to-practice-starting-code/src/App.js
+++ 16-router/09-time-to-practice-starting-code/src/App.js
@@ -1,28 +1,33 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-
+import React, { Suspense } from "react";
 import AllQuotes from "./pages/AllQuotes";
-import CreateQuotes from "./pages/CreateQuote";
+
 import QuoteDetails from "./pages/QuoteDetails";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const CreateQuotes = React.lazy(() => import('./pages/CreateQuote'))
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
-        </Route>
-        <Route path='/quotes' exact>
-          <AllQuotes />
-        </Route>
-        <Route path='/quotes/:id'>
-          <QuoteDetails />
-        </Route>
-        <Route path='/create' >
-          <CreateQuotes />
-        </Route>
-        <Route path='*' render={() => <h1 style={{ textAlign: 'center', color: '#008080' }}>No page found!!!</h1>}/>
-      </Switch>
+      <Suspense fallback={<div className="centered"><LoadingSpinner /></div>}>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes' />
+          </Route>
+          <Route path='/quotes' exact>
+            <AllQuotes />
+          </Route>
+          <Route path='/quotes/:id'>
+            <QuoteDetails />
+          </Route>
+          <Route path='/create' >
+            <CreateQuotes />
+          </Route>
+          <Route path='*' render={() => <h1 style={{ textAlign: 'center', color: '#008080' }}>No page found!!!</h1>} />
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
