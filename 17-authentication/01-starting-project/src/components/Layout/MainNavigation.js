fdaf12b1aa374f39449450ff97d81c70f9f332d3
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-10-23 14:08:16 +0300

    create route guards

--- 17-authentication/01-starting-project/src/components/Layout/MainNavigation.js
+++ 17-authentication/01-starting-project/src/components/Layout/MainNavigation.js
@@ -1,11 +1,12 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import AuthContext from '../../store/AuthContext';
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
   const ctx = useContext(AuthContext);
+  const history = useHistory();
 
   return (
     <header className={classes.header}>
@@ -21,7 +22,10 @@ const MainNavigation = () => {
           {ctx.isLogin && <li><Link to='/profile'>Profile</Link> </li>}
 
 
-          {ctx.isLogin && <li> <button>Logout</button></li>}
+          {ctx.isLogin && <li> <button onClick={()=> {
+            ctx.logout();
+            history.replace('/')
+          }}>Logout</button></li>}
 
         </ul>
       </nav>
