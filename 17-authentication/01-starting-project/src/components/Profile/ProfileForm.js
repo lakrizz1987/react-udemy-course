66eb7f46b796cb7ac981760c9cfe179ef6339cc5
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-10-23 13:28:53 +0300

    using token to change pass

--- 17-authentication/01-starting-project/src/components/Profile/ProfileForm.js
+++ 17-authentication/01-starting-project/src/components/Profile/ProfileForm.js
@@ -1,11 +1,37 @@
+import { useContext, useRef } from 'react';
+import AuthContext from '../../store/AuthContext';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const passRef = useRef();
+  const ctx = useContext(AuthContext);
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+
+    const newPassword = passRef.current.value;
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBrUgTAteNBRQgdJXs_3OXUjh3hzPY_8ac',
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({ idToken: ctx.token, password: newPassword, returnSecureToken: false }),
+      }
+    )
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => {
+        alert(err)
+      })
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength='7' ref={passRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
