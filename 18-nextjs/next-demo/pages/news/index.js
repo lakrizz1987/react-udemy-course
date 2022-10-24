cac1e981314ceb544f7f224018bba391906257d1
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-24 11:59:59 +0300

    using Link

--- 18-nextjs/next-demo/pages/news/index.js
+++ 18-nextjs/next-demo/pages/news/index.js
@@ -1,5 +1,17 @@
-const NewsPage = () =>{
-  return <h1>News Page</h1>
+import Link from "next/link";
+import { Fragment } from "react";
+
+const NewsPage = () => {
+  return (
+    <Fragment>
+      <h1>News Page</h1>
+      <ul>
+        <li><Link href='/news/next'>Go to NextJS!</Link></li>
+        <li><Link href='/news/react'>Go to ReactJS!</Link></li>
+      </ul>
+
+    </Fragment>
+  )
 }
 
 export default NewsPage;
