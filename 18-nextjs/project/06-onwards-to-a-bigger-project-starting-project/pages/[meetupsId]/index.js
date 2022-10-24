51f68999fa1aa86846fe68dbbcdc1600f1744147
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-10-24 20:14:13 +0300

    intro to getStaticPaths

--- 18-nextjs/project/06-onwards-to-a-bigger-project-starting-project/pages/[meetupsId]/index.js
+++ 18-nextjs/project/06-onwards-to-a-bigger-project-starting-project/pages/[meetupsId]/index.js
@@ -1,12 +1,47 @@
 import MeetupDetails from "../../components/meetups/MeetupDetails"
 
-const MeetupDetailsPage = () => {
+const MeetupDetailsPage = (props) => {
     return <MeetupDetails
         image='https://upload.wikimedia.org/wikipedia/en/thumb/7/7e/Windhill.jpg/1024px-Windhill.jpg'
         title='First meetup'
         description='This is some description!'
-        address = 'Some Str.5, Some City'
+        address='Some Str.5, Some City'
     />
+}
+
+export async function getStaticPaths(){
+    return{
+        fallback: false,
+        paths:[
+            {
+                params:{
+                    meetupsId: 'p1'
+                }
+            },
+            {
+                params:{
+                    meetupsId: 'p2'
+                }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+
+    const id = context.params.meetupsId;
+    console.log(id)
+
+    return {
+        props: {
+            meetupDetails: {
+                image: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/7e/Windhill.jpg/1024px-Windhill.jpg',
+                title: 'First meetup',
+                description: 'This is some description!',
+                address: 'Some Str.5, Some City'
+            }
+        }
+    }
 }
 
 export default MeetupDetailsPage;
