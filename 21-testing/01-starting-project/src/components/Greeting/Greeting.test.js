605739155a2cf28eb5419083986acdc0b5431c8a
Ivaylo <ignatov.ivaylo@gmail.com>
2022-10-27 11:59:16 +0300

    creating suites

--- 21-testing/01-starting-project/src/components/Greeting/Greeting.test.js
+++ 21-testing/01-starting-project/src/components/Greeting/Greeting.test.js
@@ -1,8 +1,17 @@
-import { render,screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting"
 
-test('is Hello World rendering', () => {
-    render(<Greeting />);
-    const element = screen.getByText('Hello World',{exact:false});
-    expect(element).toBeInTheDocument();
+
+describe('Greeting component', () => {
+    test('is Hello World rendering', () => {
+        render(<Greeting />);
+        const element = screen.getByText('Hello World', { exact: false });
+        expect(element).toBeInTheDocument();
+    })
+
+    test('is Hello World rendering', () => {
+        render(<Greeting />);
+        const element = screen.getByText('some text', { exact: false });
+        expect(element).toBeInTheDocument();
+    })
 })
