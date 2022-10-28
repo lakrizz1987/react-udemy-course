3de1ffa1516c985212eca7ea4d3d78032c57c67a
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-10-28 08:58:21 +0300

    Update basics.ts

--- 22-typescript/01-starting-project/basics.ts
+++ 22-typescript/01-starting-project/basics.ts
@@ -11,3 +11,30 @@ age = 12;
 let isStudy: boolean;
 
 isStudy = true;
+
+//Arrays Objects
+
+let array: string[];
+
+array = ['Ivo', 'Pesho']
+
+let preson: {
+    name: string;
+    age: number;
+    isStudy: boolean;
+};
+
+preson = {
+    name: 'Ivo',
+    age: 34,
+    isStudy: true
+}
+
+let arrayPersons: {
+    name: string;
+    age: number;
+    isStudy: boolean;
+}[];
+
+arrayPersons = [];
+arrayPersons.push(preson);
