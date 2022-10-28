a3b80faad85dc7766b4434c193cbe7ce63ff5e20
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-10-28 11:28:26 +0300

    crate functional components with ts

--- 22-typescript/01-starting-project/react-ts/my-app/src/App.tsx
+++ 22-typescript/01-starting-project/react-ts/my-app/src/App.tsx
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/Todo';
 
 function App() {
+  const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+
   return (
     <div>
-      <h1>helooooo</h1>
+      <Todos items={todos}/>
     </div>
   );
 }
--- 22-typescript/01-starting-project/react-ts/my-app/src/components/Todos.tsx
+++ 22-typescript/01-starting-project/react-ts/my-app/src/components/Todos.tsx
@@ -1,3 +1,13 @@
-function Todos(){};
+import React from "react";
+import Todo from "../models/Todo";
+import TodoItem from "./TodoItem";
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+    return (
+        <ul>
+            {props.items.map(item => <TodoItem text={item.text} key={item.id} />)}
+        </ul>
+    )
+};
 
 export default Todos;
